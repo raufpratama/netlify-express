9df4c08422e02a4694a071a3f57c3adafad474fa
raufpratama <pratamarauf8@gmail.com>
2020-01-31 13:37:57 +0700

    cors

--- util/cors.config.js
+++ util/cors.config.js
@@ -1,5 +1,5 @@
 let corsOptions = {
-    origin: ['https://test-eatsyapp.netlify.com',/\.test-eatsyapp.netlify\.com$/,'https://eatsyapp.co',/\.eatsyapp\.co$/],
+    origin: ['https://test-eatsyapp.netlify.com',/\.test-eatsyapp.netlify\.com$/,'https://eatsyapp.co',/\.eatsyapp\.co$/,'http://localhost:3000'],
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
